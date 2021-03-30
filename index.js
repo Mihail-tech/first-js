73fde8603d413ba864553ff435f7528b86dce309
Mihail-tech <78023617+Mihail-tech@users.noreply.github.com>
2021-03-30 22:31:40 +0300

    added 6 task

---
+++ index.js
@@ -0,0 +1,118 @@
+const arr = [`1 задача`]
+console.log(arr)
+
+const str = "привет"
+console.log(typeof str)
+const num = 123
+console.log(typeof num )
+const flag = true
+console.log(typeof flag)
+const txt = "true"
+console.log(typeof txt)
+
+const arr1 = [`2 задача`]
+console.log(arr1)
+
+let a1 = 5 + 3
+let a2 = 5 - 3
+let a3 = 5 * 3
+let a4 = 5 / 3
+console.log(a1, a2, a3, a4)
+
+const arr2 = [`3 задача`]
+console.log(arr2)
+
+let a6 = 5 % 3
+console.log(a6)
+let a7 = 3 % 5
+console.log(a7)
+let a8 = 5 + '3'
+console.log(a8)
+let a9 = '5' - 3
+console.log(a9)
+let a10 = 75 + 'кг'
+console.log(a10)
+
+
+
+const arr3 = [`4 задача`]
+console.log(arr3)
+
+let width = 10
+let height = 23
+let s = width * height
+console.log(s)
+
+
+
+
+
+
+const arr4 = [`5 задача`]
+console.log(arr4)
+
+
+const п = 3.14 / 4
+console.log(п)
+
+let dC = 4
+let D = Math.pow(dC, 2)
+console.log(D)
+
+let h = 10
+console.log(h)
+
+let V = п * D * h
+console.log(V, 'ответ')
+let result = Math.round(V)
+console.log(result, 'округленный ответ')
+
+
+
+
+
+
+const arr5 = [`6 задача`]
+console.log(arr5)
+
+let a = 3
+let b = 4
+
+let k = Math.pow(a, 2) + Math.pow(b, 2)
+console.log(k, 'с корнем')
+let sqrt = Math.sqrt(k)
+console.log(sqrt, 'без корня')
+
+
+
+
+
+const arr6 = [`7 задача`]
+console.log(arr6)
+
+let nFib = 12
+
+/* первая скобка*/
+const sqrtFive = 5
+let p = Math.sqrt(sqrtFive)
+console.log(p, 'корень из 5')
+
+let divis = (1 + p) / 2
+console.log(divis, 'деление в скобке 1')
+
+let pow = Math.pow(divis, nFib)
+console.log(pow, 'результат 1 скоб')
+
+/*вторая скобка*/
+let divis1 = (1 - p) / 2
+console.log(divis1, 'деление в скобке 2')
+
+let pow1 = Math.pow(divis1, nFib)
+console.log(pow1, 'результат 2 скоб')
+
+/*общая формула*/
+let Fn = (pow - pow1) / a
+console.log(Fn, 'ответ')
+
+let roud = Math.round(Fn)
+console.log(roud, 'округ. ответ')
